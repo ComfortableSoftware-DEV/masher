70e0584ddab2cbb2ec413615cd806b1588719ef8
GaelicGrime <will.angus.blaylock@gmail.com>
2021-05-21 20:45:14 -0600

    initial commit
    geterstarted

---
+++ masher.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+import gc
+
+
+import CF
+import DO
+
+
+gc.enable()
+
+
+# #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+# __main__
+# #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+def __main__():
+	# fold here ⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1
+	DO.checkDeviceConnect()
+	# print(f"{CF.CLRALL}")
+	while True:
+		try:
+			for thisDevice_ in DO.DEVICES:
+				DO.localTime()
+				DO.queueEvents(thisDevice_)
+				DO.dispatchEvents(thisDevice_)
+		except DO.LD.EventsDroppedException:
+			print(f"dropped events{CF.NEWLINE}")
+		except OSError as e:
+			if e.strerror == 'No such device':
+				# handle lost device
+				print("lost device")
+		except KeyboardInterrupt:
+			exit()
+
+	# fold here ⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1⥣1
+
+
+if __name__ == "__main__":
+	__main__()
